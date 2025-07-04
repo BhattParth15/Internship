5c191f4d177f28eab9d8c94800bef43d1d02ca3e
Parth Bhatt <parth15bhat@gmail.com>
2025-07-04 19:11:52 +0530

    Update WebForm1.aspx.cs

--- Day3/WebForm1.aspx.cs
+++ Day3/WebForm1.aspx.cs
@@ -1,1 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
+namespace LogingWeb
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            String username = txtUser.Text;
+            String password = txtPass.Text;
+
+            if (username == "admin" && password == "admin")
+            {
+                Response.Write("<script type='text/javascript'>alert('Login Successful');</script>");
+            }
+            else
+            {
+                Response.Write("<script type='text/javascript'>alert('Login Error');</script>");
+            }
+        }
+    }
+}
