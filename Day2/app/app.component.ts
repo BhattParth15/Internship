30dccca8d097c3d356082cbe624f3b51d361f0b7
Parth Bhatt <parth15bhat@gmail.com>
2025-07-04 12:29:24 +0530

    Update app.component.ts

--- Day2/app/app.component.ts
+++ Day2/app/app.component.ts
@@ -3,8 +3,8 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,  // ✅ required if you're bootstrapping it directly
-  imports: [RouterModule],  // ✅ needed for router-outlet to work
+  standalone: true,  //  required if you're bootstrapping it directly
+  imports: [RouterModule],  // needed for router-outlet to work
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
